b65f4ed408fc722d8a12dff696ab0653acb0e713
Eli <elitward@gmail.com>
2015-03-05 13:28:45 -0800

    add DataSample & del .idea

---
+++ app/src/main/java/ca/ubc/icics/mss/cisc530/DataSample.java
@@ -0,0 +1,21 @@
+package ca.ubc.icics.mss.cisc530;
+
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.Date;
+
+/**
+ * Created by Elitward on 3/5/2015.
+ */
+public class DataSample {
+    //raw info
+    String name;
+    String details;
+    Float value;
+    Date time;
+    LatLng location;
+
+    //additional info
+    BitmapDescriptor icon;
+}
